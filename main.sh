531a77dd7b3f23988291ff2dbd1e2de48ef3a403
Mael-GN <mael.grelneau@gmail.com>
2023-04-14 16:14:17 +0200

    traitement files

--- main.sh
+++ main.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+FICHIER=`cat accounts.csv`
+for data in $FICHIER 
+do
+    echo $data
+    NAME=`echo $data | awk -F";"`
+    SURMANE=`echo $data | awk -F";"`
+    MAIL=`echo $data | awk -F";"`
+    PASSWORD=`echo $data | awk -F";"`
+    echo "$NAME - $SURNAME - $MAIL - $MAIL - $PASSWORD"
+done
