060b6a10edfd7a77eee7dad58e27de15ae3c77ac
Mael-GN <mael.grelneau@gmail.com>
2023-05-09 14:22:16 +0200

    Découpe des données

--- main.sh
+++ main.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-FICHIER=`cat accounts.csv`
-for data in $FICHIER 
-do
-    echo $data
-    NAME=`echo $data | awk -F";"`
-    SURMANE=`echo $data | awk -F";"`
-    MAIL=`echo $data | awk -F";"`
-    PASSWORD=`echo $data | awk -F";"`
-    echo "$NAME - $SURNAME - $MAIL - $MAIL - $PASSWORD"
-done
+while IFS=";" read -r NAME SURNAME MAIL PASSWORD; do
+    echo "$NAME"
+    echo "$SURNAME"
+    echo "$MAIL"
+    echo "$PASSWORD"
+done <"accounts.csv"
