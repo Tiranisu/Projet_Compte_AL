e85786e59778e697e7909024a8ffb7b1d427af51
Mael-GN <mael.grelneau@gmail.com>
2023-05-09 16:29:03 +0200

    main.sh création utilisateur

--- main.sh
+++ main.sh
@@ -8,8 +8,18 @@
 #Séparateur 
 #Utilisation du -r avec read pour ne pas interpréter les caractères d'échappement (exemple avec l'antislash)
 while IFS=";" read -r name surname mail password; do
-    echo "$name - $surname"
+    echo -e "\n$name - $surname - $password"
     login="${name:0:1}${surname// /}" #On prend 1 lettre à l'index 0 du nom, puis on rajoute le nom sans les espaces pour les nom composés
-    echo -e "$login\n" #Utilisation du -e pour appliquer l'antislash (plus lisible dans la console)
+    echo "$login" #Utilisation du -e pour appliquer l'antislash (plus lisible dans la console)
 
+    sudo useradd -m "$login" -p"\"$password\""
+    #sudo userdel -r $login
+    
+    if [ ! -d "/home/$login/a_sauver" ]; then
+        sudo mkdir /home/$login/a_sauver
+    fi
+
+
+    sudo mkdir /home/shared
+    sudo chown root /home/shared
 done <"accounts.csv"
