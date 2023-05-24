cf9ba8083ebe1a611a280bf162e5a9a7a648acd6
Mael-GN <mael.grelneau@gmail.com>
2023-05-24 17:18:27 +0200

    Sauvegarde crontab finish

--- retablir_sauvegarde.sh
+++ retablir_sauvegarde.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+#Récupère le fichier de sauvegarde sur le serveur distant compréssé
+scp -i /home/isen/.ssh/id_rsa mgrell25@10.30.48.100:/home/saves/$1
+
+
+#Décompresse le fichier de sauvegarde
+tar -xzvf save_$1.tgz
+
+#Déplacer le fichier de sauvegarde a la place du fichier a_sauver actuel
