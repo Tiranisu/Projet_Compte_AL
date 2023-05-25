d4688c862603ca36ad3ba8caf44bc439e244e6e4
Mael-GN <mael.grelneau@gmail.com>
2023-05-25 23:08:35 +0200

    a tester fichier rétablir save

--- retablir_sauvegarde.sh
+++ retablir_sauvegarde.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+username="mgrell25"
+server_ip="10.30.48.100"
+rsa_key="/home/isen/.ssh/id_rsa"
+
+
 #Récupère le fichier de sauvegarde sur le serveur distant compréssé
-scp -i /home/isen/.ssh/id_rsa mgrell25@10.30.48.100:/home/saves/$1
+save_path="/home/saves/$1.tgz"
+scp -i $rsa_key $username@$server_ip:$save_path
+
 
 
 #Décompresse le fichier de sauvegarde
 tar -xzvf save_$1.tgz
 
 #Déplacer le fichier de sauvegarde a la place du fichier a_sauver actuel
+rm /home/$1/a_sauver
+mv home /home/
+rm -r home
+rm save_$1.tgz
